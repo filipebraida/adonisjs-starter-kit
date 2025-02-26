44bfa3cd68ee906f26f057e8d6a36b1dbae4e510
Filipe Braida <filipebraida@gmail.com>
2025-02-26 11:41:13 -0300

    feat: enhance error pages with improved layout and navigation options

--- apps/web/app/core/exceptions/handler.ts
+++ apps/web/app/core/exceptions/handler.ts
@@ -22,8 +22,8 @@ export default class HttpExceptionHandler extends ExceptionHandler {
    * to return the HTML contents to send as a response.
    */
   protected statusPages: Record<StatusPageRange, StatusPageRenderer> = {
-    '404': (error, { inertia }) => inertia.render('errors/not_found', { error }),
-    '500..599': (error, { inertia }) => inertia.render('errors/server_error', { error }),
+    '404': (error, { inertia }) => inertia.render('core/errors/not_found', { error }),
+    '500..599': (error, { inertia }) => inertia.render('core/errors/server_error', { error }),
   }
 
   /**
