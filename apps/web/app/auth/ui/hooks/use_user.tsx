816fdb1cdfda22f50e357f6ef727c09e97e71bba
Filipe Braida <filipebraida@gmail.com>
2025-02-25 21:05:44 -0300

    refactor: change User import to type import for better type safety

--- apps/web/app/auth/ui/hooks/use_user.tsx
+++ apps/web/app/auth/ui/hooks/use_user.tsx
@@ -1,5 +1,5 @@
 import usePageProps from '#common/ui/hooks/use_page_props'
-import User from '#users/models/user'
+import type User from '#users/models/user'
 
 export default function useUser() {
   const { user } = usePageProps<{ user: User }>()
