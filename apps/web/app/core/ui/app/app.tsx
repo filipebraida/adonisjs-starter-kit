aaed31226a29ab039ba76505553c97f1448bd079
Filipe Braida <filipebraida@gmail.com>
2025-06-08 15:43:15 -0300

    feat: integrate i18n support with i18next and react-i18next

--- apps/web/app/core/ui/app/app.tsx
+++ apps/web/app/core/ui/app/app.tsx
@@ -9,6 +9,8 @@ import { TuyauProvider } from '@tuyau/inertia/react'
 import { tuyau } from './tuyau'
 
 import { isSSREnableForPage } from 'config/ssr'
+import { setupI18n } from '../config/i18n.config'
+import { I18nextProvider } from 'react-i18next'
 
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS Starter Kit'
 
@@ -17,7 +19,7 @@ createInertiaApp({
 
   title: (title) => (title ? `${title} - ${appName}` : appName),
 
-  resolve: (name) => {
+  resolve: async (name) => {
     const firstPart = name.split('/')[0]
     const rest = name.split('/').slice(1).join('/')
     return resolvePageComponent(
@@ -26,22 +28,28 @@ createInertiaApp({
     )
   },
 
-  setup({ el, App, props }) {
+  setup: ({ el, App, props }) => {
     const componentName = props.initialPage.component
     const isSSREnabled = isSSREnableForPage(componentName)
+
+    const i18nInstance = setupI18n({ locale: 'pt' })
 
     if (isSSREnabled) {
       hydrateRoot(
         el,
-        <TuyauProvider client={tuyau}>
-          <App {...props} />
-        </TuyauProvider>
+        <I18nextProvider i18n={i18nInstance}>
+          <TuyauProvider client={tuyau}>
+            <App {...props} />
+          </TuyauProvider>
+        </I18nextProvider>
       )
     } else {
       createRoot(el).render(
-        <TuyauProvider client={tuyau}>
-          <App {...props} />
-        </TuyauProvider>
+        <I18nextProvider i18n={i18nInstance}>
+          <TuyauProvider client={tuyau}>
+            <App {...props} />
+          </TuyauProvider>
+        </I18nextProvider>
       )
     }
   },
---
+++ apps/web/app/core/ui/config/i18n.config.ts
@@ -0,0 +1,60 @@
+import i18n from 'i18next'
+import ICU from 'i18next-icu'
+import { initReactI18next } from 'react-i18next'
+
+import USERS_EN from '../../../../resources/lang/en/users.json' with { type: 'json' }
+import USERS_PT from '../../../../resources/lang/pt/users.json' with { type: 'json' }
+
+let i18nInstance: typeof i18n | null = null
+
+export const setupI18n = ({
+  locale,
+  fallbackLocale = 'en',
+}: {
+  locale: string
+  fallbackLocale?: string
+}) => {
+  if (
+    i18nInstance?.isInitialized &&
+    i18nInstance.language === locale &&
+    i18nInstance.options?.fallbackLng === fallbackLocale
+  ) {
+    return i18nInstance
+  }
+
+  const config = {
+    resources: {
+      pt: {
+        translation: {
+          users: USERS_PT,
+        },
+      },
+      en: {
+        translation: {
+          users: USERS_EN,
+        },
+      },
+    },
+    lng: locale,
+    fallbackLng: fallbackLocale,
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    load: 'currentOnly',
+    returnNull: false,
+    returnEmptyString: false,
+    supportedLngs: ['pt', 'en'],
+    react: {
+      useSuspense: false,
+    },
+  } as const
+
+  i18nInstance = i18n.createInstance().use(ICU).use(initReactI18next)
+
+  i18nInstance.init(config)
+
+  return i18nInstance
+}
+
+export const getI18nInstance = () => i18nInstance
