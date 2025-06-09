afe1c731c665b869f4dc118592f5d81ce13afdcf
Filipe Braida <filipebraida@gmail.com>
2025-06-09 11:43:07 -0300

    feat: integrate i18n support in SSR setup for localized rendering

--- apps/web/app/core/ui/app/ssr.tsx
+++ apps/web/app/core/ui/app/ssr.tsx
@@ -3,6 +3,9 @@ import { createInertiaApp } from '@inertiajs/react'
 import { TuyauProvider } from '@tuyau/inertia/react'
 import { tuyau } from './tuyau'
 
+import { setupI18n } from '../config/i18n.config'
+import { I18nextProvider } from 'react-i18next'
+
 export default function render(page: any) {
   return createInertiaApp({
     page,
@@ -14,10 +17,21 @@ export default function render(page: any) {
       const pages = import.meta.glob('/app/*/ui/pages/**/*.tsx', { eager: true })
       return pages[`/app/${firstPart}/ui/pages/${rest}.tsx`]
     },
-    setup: ({ App, props }) => (
-      <TuyauProvider client={tuyau}>
-        <App {...props} />
-      </TuyauProvider>
-    ),
+    setup: ({ App, props }) => {
+      const { locale, fallbackLocale } = props.initialPage.props as unknown as {
+        locale: string
+        fallbackLocale?: string
+      }
+
+      const i18nInstance = setupI18n({ locale, fallbackLocale })
+
+      return (
+        <I18nextProvider i18n={i18nInstance}>
+          <TuyauProvider client={tuyau}>
+            <App {...props} />
+          </TuyauProvider>
+        </I18nextProvider>
+      )
+    },
   })
 }
