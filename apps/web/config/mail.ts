2f994dca3c03ef3849fd4d9b612f12a6103ad0ee
Lupiac <antoine.lupiac0@gmail.com>
2025-07-09 15:29:02 +0200

    feat: add smtp provider + mailpit container + pgadmin container

--- apps/web/config/mail.ts
+++ apps/web/config/mail.ts
@@ -10,6 +10,29 @@ const mailConfig = defineConfig({
    * options.
    */
   mailers: {
+    smtp: transports.smtp({
+      host: env.get('SMTP_HOST'),
+      port: env.get('SMTP_PORT'),
+      secure: Boolean(env.get('SMTP_SECURE')),
+
+      auth: {
+        type: 'login',
+        user: env.get('SMTP_USERNAME'),
+        pass: env.get('SMTP_PASSWORD'),
+      },
+
+      tls: {
+        rejectUnauthorized: Boolean(env.get('SMTP_SECURE')),
+      },
+
+      ignoreTLS: false,
+      requireTLS: false,
+
+      pool: false,
+      maxConnections: 5,
+      maxMessages: 100,
+    }),
+
     resend: transports.resend({
       key: env.get('RESEND_API_KEY'),
       baseUrl: 'https://api.resend.com',
--- apps/web/start/env.ts
+++ apps/web/start/env.ts
@@ -45,6 +45,10 @@ export default await Env.create(new URL('../', import.meta.url), {
   */
   RESEND_API_KEY: Env.schema.string(),
   EMAIL_FROM: Env.schema.string(),
+  SMTP_HOST: Env.schema.string({ format: 'host' }),
+  SMTP_PORT: Env.schema.number(),
+  SMTP_USERNAME: Env.schema.string(),
+  SMTP_PASSWORD: Env.schema.string(),
 
   /*
   |----------------------------------------------------------
