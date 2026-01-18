33b5ce712145f091d99ebaa51ab3efe9b8da6cfd
Maxime <57860498+MaximeMRF@users.noreply.github.com>
2026-01-18 14:38:45 +0200

    fix: tuyau base url (#19)

--- apps/web/app/core/ui/app/client.ts
+++ apps/web/app/core/ui/app/client.ts
@@ -2,7 +2,9 @@ import { registry } from '@generated/registry'
 import { createTuyau } from '@tuyau/core/client'
 
 export const client = createTuyau({
-  baseUrl: import.meta.env.VITE_API_URL || `http://localhost:3333`,
+  baseUrl:
+    import.meta.env.VITE_API_URL ||
+    (typeof window !== 'undefined' ? window.location.origin : `http://localhost:3333`),
   registry,
 })
 
